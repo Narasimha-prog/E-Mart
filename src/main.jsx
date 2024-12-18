15b09f26f08fe4c8b9bd9e890635889e113b01a2
Narasimha-prog <lnreddyreddy405@gmail.com>
2024-12-18 21:42:50 +0530

    Normalize line endings

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'  // Use HashRouter instead of BrowserRouter
 import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <StrictMode>
-    <CartProvider>
-    <App />
-    </CartProvider>
-  </StrictMode>
-  </BrowserRouter>
+  <HashRouter>  {/* Use HashRouter */}
+    <StrictMode>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </StrictMode>
+  </HashRouter>
 )
