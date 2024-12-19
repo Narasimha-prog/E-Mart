a4fe6d05cea565409d8e188ce0964a4d5e7a8c06
Narasimha-prog <lnreddyreddy405@gmail.com>
2024-12-19 16:53:00 +0530

    add: deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'  // Use HashRouter instead of Brow
 import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename='/E-Mart'>  {/* Use HashRouter */}
+  <HashRouter>  {/* Use HashRouter */}
     <StrictMode>
       <CartProvider>
         <App />
